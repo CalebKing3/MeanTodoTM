f2095e394dcf37da64764722f061df3086da7ce2
Caleb King <calebking3@gmail.com>
2016-03-09 10:23:48 -0700

    Creating an API controller

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+
+var router = require('./api');
+
+var app = express();
+
+app.use('/', express.static('public'));
+
+app.use('/api', router);
+
+app.listen(1738, function(){
+  console.log("The server is running and Fetty Wap is singing!");
+});
